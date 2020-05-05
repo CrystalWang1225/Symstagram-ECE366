7d007e19677469a8abf971dcb0882624daca1c28
bchung2017 <chung3@cooper.edu>
2020-05-05 17:20:37 -0400

    java servlet example

---
+++ testsession1/src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,11 @@
+package servlet;
+
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
